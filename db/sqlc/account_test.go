6f03e50391a8d1622f6c1bfae01bc638b19b34cb
dungpt <phamtandung098@gmail.com>
2023-10-09 00:20:41 +0700

    fix

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -58,13 +58,12 @@ func TestUpdateAccount(t *testing.T) {
 	}
 
 	account2, err := testQueries.updateAcount(context.Background(), arg)
-
 	require.NoError(t, err)
 	require.NotEmpty(t, account2)
 
 	require.Equal(t, account1.ID, account2.ID)
 	require.Equal(t, account1.Owner, account2.Owner)
-	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, arg.Balance, account2.Balance)
 	require.Equal(t, account1.Currency, account2.Currency)
 	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
